68d8d93fe8043ce16b29d70ea84d803758c27600
samly97 <97.sam.ly@gmail.com>
2020-09-11 12:01:51 -0400

    Add comments about how to use CORS mw. Included word of caution when user authentication is required

--- cors.go
+++ cors.go
@@ -5,34 +5,43 @@ import (
 	"strings"
 )
 
-// c := cors.New(cors.Options{
-// 	AllowCredentials: true,
-// 	AllowedOrigins:   []string{"http://localhost:3000"},
-// 	AllowedMethods:   []string{"GET", "POST"},
-// 	AllowedHeaders: []string{"Origin", "Content-Type",
-// 		"X-Auth-Token"},
-// })
-
-// CORS is
+// The CORS middleware will apply CORS headers to requests which require
+// CORS. Headers are attached to the preflight requests and to the actual
+// requests as well.
+//
+// The user would have to specify the headers that are applied in the
+// 'New' initialization function. The user could find a list of functional
+// options available from functions that returns the 'corsOption' function
+// type.
+//
+// Note: for requests that require user authentication, the CORS
+// middleware may need to be wrapped around the users middlware first and
+// not the other way around.
+//
+// This is because CORS need to send preflights before the actual request
+// goes through. So if your user authentication needs to pass cookies
+// for authentication, then they aren't attached during the preflight
+// and depending on how your user auth middleware is implemented (early
+// exit if nil cookie), then we may not even reach the CORS implementation
+// at all.
+//
+// E.g.:
+// Do this...
+//	corsMw.ApplyFn(userAuthMw.ApplyFn(...))
+// Not this...
+//	userAuthMw.ApplyFn(corsMw.ApplyFn(...))
 type CORS struct {
-	Options map[string]string
+	Headers map[string]string
 }
 
+// New initializes a CORS object with the options specified by the
+// variadic corsOptions function.
 func New(opts ...corsOption) CORS {
 	cors := CORS{Options: make(map[string]string)}
 	for _, opt := range opts {
 		opt(&cors)
 	}
 	return cors
-}
-
-type corsOption func(*CORS)
-
-func AllowOrigins(origins []string) corsOption {
-	return func(cors *CORS) {
-		s := strings.Join(origins, ", ")
-		cors.Options["Access-Control-Allow-Origin"] = s
-	}
 }
 
 func (c *CORS) Apply(next http.Handler) http.HandlerFunc {
@@ -64,3 +73,17 @@ func (c *CORS) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 		}
 	}
 }
+
+//
+type corsOption func(*CORS)
+
+func AllowOrigins(origins []string) corsOption {
+	return func(cors *CORS) {
+		s := strings.Join(origins, ", ")
+		cors.Options["Access-Control-Allow-Origin"] = s
+	}
+}
+
+// 	AllowCredentials: true,
+// 	AllowedMethods:   []string{"GET", "POST"},
+// 	AllowedHeaders: []string{"Origin", "Content-Type",
