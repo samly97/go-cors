3120bd49b15a3fa909a938b6376dda3cdf430f76
samly97 <97.sam.ly@gmail.com>
2020-09-11 10:38:57 -0400

    initial commit

---
+++ cors.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+type Cors struct {
+}
+
+func (c *Cors) Apply(next http.Handler) http.HandlerFunc {
+	return c.ApplyFn(next.ServeHTTP)
+}
+
+func (c *Cors) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			// Set Prelight headers here
+			w.Header().Set("Access-Control-Allow-Origin",
+				"http://localhost:3000")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+			w.Header().Set("Access-Control-Allow-Headers",
+				"Content-Type, Origin")
+			w.Header().Set("Access-Control-Max-Age", "300")
+			return
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin",
+				"http://localhost:3000")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+			w.Header().Set("Access-Control-Allow-Headers",
+				"Content-Type, Origin")
+			w.Header().Set("Access-Control-Max-Age", "300")
+			// Forward to non-simple request
+			next(w, r)
+		}
+	}
+}
